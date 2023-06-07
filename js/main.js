84adcced39475f32abc63ffd56f976efbdcb7746
nicklllson <nicklllson.dev@gmail.com>
2023-06-08 00:52:17 +0500

    Закончил проект

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 const button = document.querySelector(".burger");
 const lines = document.querySelectorAll('.burger__lines');
+const burger = document.querySelector('.burger');
+const burgerClose = document.querySelector('.close-btn');
+const menu = document.querySelector('.popup-menu');
+const body = document.querySelector('.body');
+
+burger.addEventListener('click', () => {
+  menu.classList.add('popup-menu--active');
+  body.classList.add('body-active');
+});
+
+burgerClose.addEventListener('click', () => {
+  menu.classList.remove('popup-menu--active');
+  body.classList.remove('body-active');
+});
 
 lines.forEach((lines) => {
   button.addEventListener("click", function() {
